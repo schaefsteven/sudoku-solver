51b504086d28376b7990f15ff03c4cfb256d3a52
Steven Schaefer <schaefsteven@gmail.com>
2022-09-16 01:10:18 -0500

    About to make some major changes to the divider lines. Commiting to save this point in time.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget 
 from kivy.uix.textinput import TextInput
 from kivy.properties import StringProperty, ObjectProperty
+from kivy.graphics import Line, Color
 
 from sudoku import sudoku
 
@@ -21,7 +22,34 @@ class CellGrid(GridLayout):
         self.board = sudoku.Board()
         for cell in self.board.all_cells:
             self.add_widget(CellBox(cell))
+        self.dividers = []
         self.update()
+        # draw the divider lines
+        with self.canvas:
+            Color(0,0,0)
+            width = 4
+            for i in range(4):
+                self.dividers.append(Line(points=[self.x, self.y, 200, 200]))
+
+            top = self.y + self.height
+            bottom = self.y
+            left = self.x
+            right = self.x + self.width
+            for i in range(1,3):
+                x = left + (i / 3) * self.width
+                Line(points = [x, top, x, bottom], width = width)
+            for i in range(1,3):
+                y = bottom + (i / 3) * self.height
+                Line(points = [left, y, right, y], width = width)
+
+            self.my_line = Line(points = [self.x, self.y, self.width, self.height])
+
+        self.bind(x = self._update_dividers, y = self._update_dividers)
+            
+    def _update_dividers(self, instance, value):
+        print("update dividers")
+        self.x = instance.x
+        self.y = instance.y
 
     def update(self):
         for cell_box in self.children:
@@ -49,6 +77,7 @@ class CellGrid(GridLayout):
             return input
         else:
             return None
+
 
 class CellBox(TextInput):
     text_property = StringProperty('')
